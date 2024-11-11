0053b118d8fd8931f135a0ef9e40626936823aa0
Serhii Honcharov <sergeygoncharov9@gmail.com>
2024-11-11 15:30:08 +0200

    (constants addition) constants for routes, companies and users were added.

---
+++ src/Constants/companies.tsx
@@ -0,0 +1,7 @@
+const companies = [
+  { id: 1, name: 'TechCorp', industry: 'Technology' },
+  { id: 2, name: 'Foodies', industry: 'Food' },
+  { id: 3, name: 'HealthPlus', industry: 'Healthcare' },
+];
+
+export {companies};
---
+++ src/Constants/routerKeys.ts
@@ -0,0 +1,10 @@
+export const routerKeys = {
+  root: "/",
+  about: "/about",
+  register: "/register",
+  login: "/login",
+  users: "/users",
+  userProfile: "/users/:id",
+  companies: "/companies",
+  companyProfile: "/companies/:id",
+};
